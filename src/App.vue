5dbfd8b708b9f93695717cd470abf136f0ada188
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-11-13 11:22:26 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink :to="{ name: 'home' }">Inicio</RouterLink>
+      <RouterLink to="/projetos">Projetos</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>Welcome to the home page!</main>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
+
+<style scoped></style>
