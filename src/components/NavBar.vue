2ff28160873a6b1b48f1c96b3264e7c09459b128
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-11-13 16:25:00 -0500

    add font awesome

---
+++ src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="flex justify-between p-3 pt-7 items-center">
+    <div class="flex justify-around gap-5 text-black text-base font-medium">
+      <router-link
+        v-for="link in links"
+        :key="link.name"
+        :to="link.path"
+        class="hover:text-blue-900 transition-colors duration-300"
+      >
+        {{ link.name }}
+      </router-link>
+    </div>
+    <div class="flex justify-between gap-3 text-xl">
+      <a href="https://github.com/marcos-nsantos" target="_blank">
+        <i class="w-6 h-6 fa-brands fa-github" />
+      </a>
+      <a href="https://www.linkedin.com/in/marcos-santos-d" target="_blank">
+        <i class="w-6 h-6 fa-brands fa-linkedin" />
+      </a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {
+      links: [
+        { name: "Home", path: "/" },
+        { name: "Projects", path: "/projects" },
+        { name: "Contact", path: "/contact" }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 
 import "@/assets/css/index.css";
+import "@/assets/fontawesome/css/fontawesome.min.css";
+import "@/assets/fontawesome/css/brands.min.css";
+import "@/assets/fontawesome/css/solid.min.css";
 
 const app = createApp(App);
 
