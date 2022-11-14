3b98fd3d09a508309a2ba62daf9bf97a4ab1c4b0
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-11-13 21:18:29 -0500

    add tech stacks

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,6 +12,24 @@
         <i class="w-6 h-6 fa-brands fa-linkedin hover:text-blue-900" />
       </a>
     </div>
+
+    <div
+      class="flex flex-col justify-center items-center mt-10 overflow-x-auto scroll-px-3 w-full"
+    >
+      <div class="flex justify-center gap-5 mt-5 text-3xl p-2">
+        <i class="w-6 h-6 fa-brands fa-python" />
+        <i class="w-6 h-6 fa-brands fa-golang" />
+        <i class="w-6 h-6 fa-brands fa-php" />
+        <i class="w-6 h-6 fa-brands fa-docker" />
+        <i class="w-6 h-6 fa-brands fa-git-alt" />
+        <i class="w-6 h-6 fa-brands fa-aws" />
+        <i class="w-6 h-6 fa-brands fa-linux" />
+        <i class="w-6 h-6 fa-brands fa-vuejs" />
+        <i class="w-6 h-6 fa-brands fa-html5" />
+        <i class="w-6 h-6 fa-brands fa-css3-alt" />
+        <i class="w-6 h-6 fa-brands fa-js-square" />
+      </div>
+    </div>
   </main>
 </template>
 
