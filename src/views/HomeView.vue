0afee1c6d361cbdeb6d762b0782de1a530fdc718
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-11-13 20:22:38 -0500

    add the home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,17 @@
 <template>
-  <main>
-    <p>Welcome</p>
+  <main
+    class="flex flex-col justify-center items-center h-screen bg-stone-200 sm:container lg:container"
+  >
+    <h1 class="text-5xl font-bold text-gray-900">Marcos Santos</h1>
+    <h2 class="text-2xl font-medium text-gray-900">Back End Developer</h2>
+    <div class="flex justify-center gap-5 mt-5 text-3xl">
+      <a href="https://github.com/marcos-nsantos" target="_blank">
+        <i class="w-6 h-6 fa-brands fa-github hover:text-blue-900" />
+      </a>
+      <a href="https://www.linkedin.com/in/marcos-santos-d" target="_blank">
+        <i class="w-6 h-6 fa-brands fa-linkedin hover:text-blue-900" />
+      </a>
+    </div>
   </main>
 </template>
 
