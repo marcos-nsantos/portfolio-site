d25c4bfa5c7c08766f5821e9a3fd21878c857d2c
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-11-16 13:32:30 -0500

    remove project and home links from navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,5 @@
 <template>
-  <nav
-    class="flex justify-between p-5 items-center bg-stone-200 sm:container lg:container absolute w-full lg:justify-around"
-  >
-    <div class="flex justify-around gap-5 text-black text-base font-medium">
-      <router-link
-        v-for="link in links"
-        :key="link.name"
-        :to="link.path"
-        class="hover:text-blue-900 transition-colors duration-300"
-      >
-        {{ link.name }}
-      </router-link>
-    </div>
-
+  <nav class="flex justify-end p-5 bg-stone-200 absolute w-full">
     <DownloadButton>Resume</DownloadButton>
   </nav>
 </template>
@@ -22,14 +9,6 @@ import DownloadButton from "@/components/DownloadButton.vue";
 export default {
   name: "NavBar",
   components: { DownloadButton },
-  data() {
-    return {
-      links: [
-        { name: "Home", path: "/" },
-        { name: "Projects", path: "/projects" },
-      ],
-    };
-  },
 };
 </script>
 
