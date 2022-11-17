759e3266356bc158f5229d2702101412d4a59c3f
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-11-17 11:08:45 -0500

    add arial label to improve user accessibility

--- src/components/TechStacksIcons.vue
+++ src/components/TechStacksIcons.vue
@@ -2,16 +2,46 @@
   <div
     class="grid grid-flow-row grid-cols-5 gap-7 text-2xl lg:grid-flow-col lg:text-3xl"
   >
-    <i class="w-6 h-6 fa-brands fa-python" />
-    <i class="w-6 h-6 fa-brands fa-golang" />
-    <i class="w-6 h-6 fa-brands fa-docker" />
-    <i class="w-6 h-6 fa-brands fa-git-alt" />
-    <i class="w-6 h-6 fa-brands fa-aws" />
-    <i class="w-6 h-6 fa-brands fa-linux" />
-    <i class="w-6 h-6 fa-brands fa-vuejs" />
-    <i class="w-6 h-6 fa-brands fa-html5" />
-    <i class="w-6 h-6 fa-brands fa-css3-alt" />
-    <i class="w-6 h-6 fa-brands fa-js-square" />
+    <i
+      class="w-6 h-6 fa-brands fa-python"
+      aria-label="Python. Programming language"
+    ></i>
+    <i
+      class="w-6 h-6 fa-brands fa-golang"
+      aria-label="Go. Programming language"
+    ></i>
+    <i
+      class="w-6 h-6 fa-brands fa-docker"
+      aria-label="Docker. Containerization platform"
+    ></i>
+    <i
+      class="w-6 h-6 fa-brands fa-git-alt"
+      aria-label="Git. Version control system"
+    ></i>
+    <i
+      class="w-6 h-6 fa-brands fa-aws"
+      aria-label="AWS. Cloud computing platform"
+    ></i>
+    <i
+      class="w-6 h-6 fa-brands fa-linux"
+      aria-label="Linux. Distribution of the Unix operating system"
+    ></i>
+    <i
+      class="w-6 h-6 fa-brands fa-vuejs"
+      aria-label="Vue. SPA JavaScript framework"
+    ></i>
+    <i
+      class="w-6 h-6 fa-brands fa-html5"
+      aria-label="HTML5. Markup language"
+    ></i>
+    <i
+      class="w-6 h-6 fa-brands fa-css3-alt"
+      aria-label="CSS3. Style sheet language"
+    ></i>
+    <i
+      class="w-6 h-6 fa-brands fa-js-square"
+      aria-label="JavaScript. Programming language"
+    ></i>
   </div>
 </template>
 
