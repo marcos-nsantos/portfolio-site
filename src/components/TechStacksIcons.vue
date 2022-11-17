4618307de17de9f06f6279ab6dddb7f80fd2fced
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-11-17 10:09:32 -0500

    run eslint

--- src/components/TechStacksIcons.vue
+++ src/components/TechStacksIcons.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="grid grid-flow-row grid-cols-5 gap-7 text-2xl lg:grid-flow-col lg:text-3xl">
+  <div
+    class="grid grid-flow-row grid-cols-5 gap-7 text-2xl lg:grid-flow-col lg:text-3xl"
+  >
     <i class="w-6 h-6 fa-brands fa-python" />
     <i class="w-6 h-6 fa-brands fa-golang" />
     <i class="w-6 h-6 fa-brands fa-docker" />
