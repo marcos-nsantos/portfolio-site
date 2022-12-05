40991cfa80c928bf30f68c3b48daafa19089a0b4
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-12-05 13:42:05 -0500

    add a component to display error messages

---
+++ src/components/BaseErrorMessage.vue
@@ -0,0 +1,19 @@
+<template>
+  <p aria-live="assertive" class="text-red-500 text-sm" :id="id">
+    <slot />
+  </p>
+</template>
+
+<script>
+export default {
+  name: "BaseErrorMessage",
+  props: {
+    id: {
+      type: [String, Number],
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
